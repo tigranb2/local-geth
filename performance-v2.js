a01f374e1a3794d22b3677604885079bac1d0334
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-03-01 00:06:37 -0500

    Add files via upload

---
+++ performance-v2.js
@@ -0,0 +1,40 @@
+// Transactions should be sent with the following sytax in order to properly measure performance:
+// eth.sendTransaction({from:<from>,to:<to>,value:1,data:web3.toHex(new Date().getTime().toString() + "00 ;")})
+// + "00 ;" is important in order to avoid the following error: "cannot unmarshal hex string of odd length"
+// the semicolon allows for use of hexToUtf8
+
+var args = require('yargs').argv; //nodejs module for command line inputs
+var connect = (args.connect === undefined) ? 'ws://10.0.0.1:8101' : args.connect; //if --connect is undefined, set it h1 addr:port
+
+var Web3 = require('web3'); //nodejs module for web3
+var web3 = new Web3();
+web3.setProvider(new web3.providers.WebsocketProvider(connect)) //connect to specified node via WebSocket
+
+fs = require('fs'); //nodejs module for writing to file system
+fs.unlink('./data/performance.txt', (_) => {}); //comment this line to keep file between uses
+
+async function checkPerformance() {
+    var latestBlock = await web3.eth.getBlockNumber();
+    var block = await web3.eth.getBlock(latestBlock, true);
+    if (block.transactions != null) {
+        var minedTxTime = new Date().getTime();
+        block.transactions.forEach(async function (tx) {
+            try{
+                var input = web3.utils.hexToUtf8(tx.input).split(" ")[0]
+                var writtenTxTime = parseInt(input)/100 //divide by 100 to get rid of extra 0s
+                var latency = minedTxTime - writtenTxTime;
+                fs.appendFileSync('./data/performance.txt', (latency.toString() + "\n"), (err) => { //synchronously writes to ./data/performance.txt
+                    if (err) {return console.log(err);}
+                });
+            } catch (error) {
+                console.log("Tx input not correctly formatted");
+            }
+        })   
+    }
+}
+
+checkPerformance();
+
+web3.eth.subscribe('newBlockHeaders', function(error, _){
+    if (!error) {checkPerformance()} 
+})

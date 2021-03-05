5b183e3126039512ba06085201540fe5bcb7d33c
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-03-05 00:10:02 -0500

    Add files via upload

--- writeData.go
+++ writeData.go
@@ -10,9 +10,13 @@ func main() {
 }
 
 func writeMetaData(connect string, msg string){
-	metaData := fmt.Sprintf("")//--exec 'eth.sendTransaction({from:eth.accounts[0],to:eth.accounts[0],value:1,data:web3.toUtf8(%v)'", connect, msg)
-	_, err := exec.Command("geth", metaData).Output()
+	metaData := fmt.Sprintf("attach %v --exec 'eth.sendTransaction({from:eth.accounts[0],to:eth.accounts[0],value:1,data:web3.toHex(%v)'", connect, msg)
+	//output, err := exec.Command("geth", metaData).CombinedOutput() // returns 'Fatal: Unable to attach to remote geth: no known transport for URL scheme "c"'
+	output, err := exec.Command("geth", metaData).CombinedOutput() // returns "invalid command: "attach ws://127.0.0.1:8101...." <--- rest of the metaData string
+
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	fmt.Println(string(output))
 }
